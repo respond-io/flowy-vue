450a329da279e4de099ee1d4143be292db1e54ca
neel.rocketbots <neel@rocketbots.io>
2021-09-17 16:03:04 +0800

    minor fix

--- src/components/ConnectorLine.vue
+++ src/components/ConnectorLine.vue
@@ -1,52 +1,53 @@
 <template>
-	<div
-		:style="styling"
-		class="flowy-line-block overflow-visible"
-		:class="{
-			'flowy-line-block--vertical': vertical,
-			'-mt-96px': verticalOffset,
-			'no-children': noChildren,
-		}"
-	>
-		<svg class="flowy-line" preserveAspectRatio fill="none" xmlns="http://www.w3.org/2000/svg">
-			<!-- line -->
-			<path :d="path" stroke="#C5CCD0" stroke-width="2px" />
+  <div
+    :style="styling"
+    class="flowy-line-block overflow-visible"
+    :class="{
+      'flowy-line-block--vertical': vertical,
+      '-mt-96px': verticalOffset,
+      'no-children': noChildren,
+      }"
+  >
+    <svg
+      class="flowy-line"
+      preserveaspectratio="none"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <!-- line -->
+      <path :d="path" stroke="#C5CCD0" stroke-width="2px" />
 
-			<!-- arrow -->
-			<path v-if="showArrow" />
-		</svg>
-	</div>
+      <!-- arrow -->
+      <path v-if="showArrow" />
+    </svg>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'ConnectorLine',
-
-	props: {
-		styling: Object,
-		vertical: Boolean,
-		verticalOffset: Boolean,
-		path: String,
-		showArrow: Boolean,
-		noChildren: Boolean,
-	},
+  name: 'ConnectorLine',
+  props: {
+    styling: Object,
+    vertical: Boolean,
+    verticalOffset: Boolean,
+    path: String,
+    showArrow: Boolean,
+  },
 };
 </script>
 
 <style lang="scss">
 .flowy-line-block {
-	position: absolute;
-	width: 100%;
-	overflow: visible;
-	pointer-events: none;
-
-	svg {
-		width: -webkit-fill-available;
-		overflow: visible;
-	}
-
-	&--vertical {
-		top: 100%;
-	}
+  position: absolute;
+  width: 100%;
+  overflow: visible;
+  pointer-events: none;
+  svg {
+    width: -webkit-fill-available;
+    overflow: visible;
+  }
+  &--vertical {
+    top: 100%;
+  }
 }
 </style>
