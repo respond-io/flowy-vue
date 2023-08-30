70ce91157c09150fd38df939947895af33ecf379
CK <ck@rocketbots.io>
2023-08-30 11:01:31 +0800

    feat: add compatible build for migration to vue 3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/flowy-vue/' : '/flowy-vue/',
-  productionSourceMap: process.env.NODE_ENV !== 'production',
-  outputDir: 'docs',
+	publicPath: process.env.NODE_ENV === 'production' ? '/flowy-vue/' : '/flowy-vue/',
+	productionSourceMap: process.env.NODE_ENV !== 'production',
+	outputDir: 'docs',
+	lintOnSave: false,
+
+	chainWebpack: config => {
+		config.resolve.alias.set('vue', '@vue/compat');
+
+		config.module
+			.rule('vue')
+			.use('vue-loader')
+			.tap(options => {
+				return {
+					...options,
+					compilerOptions: {
+						compatConfig: {
+							MODE: 2,
+						},
+					},
+				};
+			});
+	},
 };
