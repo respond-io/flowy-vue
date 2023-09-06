d7e872336e794698e66e123232805294b735350b
shoaibkhandev <shoaibkhandev@gmail.com>
2023-09-06 16:59:53 +0800

    Remove vue/compat

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,5 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/flowy-vue/' : '/flowy-vue/',
-	productionSourceMap: process.env.NODE_ENV !== 'production',
-	outputDir: 'docs',
-	lintOnSave: false,
-
-	chainWebpack: config => {
-		config.resolve.alias.set('vue', '@vue/compat');
-
-		config.module
-			.rule('vue')
-			.use('vue-loader')
-			.tap(options => {
-				return {
-					...options,
-					compilerOptions: {
-						compatConfig: {
-							MODE: 2,
-						},
-					},
-				};
-			});
-	},
+  publicPath: process.env.NODE_ENV === "production" ? "/flowy-vue/" : "/flowy-vue/",
+  productionSourceMap: process.env.NODE_ENV !== "production",
+  outputDir: "docs",
 };
