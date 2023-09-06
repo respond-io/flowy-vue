36b4c16355b74137faa1cbd072f28d982e924a8d
shoaibkhandev <shoaibkhandev@gmail.com>
2023-09-06 16:57:08 +0800

    Remove extra libraries

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import DraggablePlugin from '@hipsjs/shopify-draggable-vue';
-import Flowy from './components/Flowy.vue';
-import FlowyNode from './components/FlowyNode.vue';
-import FlowyBlock from './components/FlowyBlock.vue';
-import FlowyNewBlock from './components/FlowyNewBlock.vue';
-import FlowyDragHandle from './components/FlowyDragHandle';
+import Flowy from "./components/Flowy.vue";
+import FlowyNode from "./components/FlowyNode.vue";
+import FlowyBlock from "./components/FlowyBlock.vue";
+import FlowyNewBlock from "./components/FlowyNewBlock.vue";
+import FlowyDragHandle from "./components/FlowyDragHandle.vue";
 
 // Declare install function executed by Vue.use()
 export function install(_Vue) {
   const Vue = _Vue;
-  Vue.use(DraggablePlugin, {
-    handle: '.flowy-drag-handle',
-  });
-
-  Vue.component('Flowy', Flowy);
-  Vue.component('FlowyNode', FlowyNode);
-  Vue.component('FlowyBlock', FlowyBlock);
-  Vue.component('FlowyNewBlock', FlowyNewBlock);
-  Vue.component('FlowyDragHandle', FlowyDragHandle);
+  Vue.component("Flowy", Flowy);
+  Vue.component("FlowyNode", FlowyNode);
+  Vue.component("FlowyBlock", FlowyBlock);
+  Vue.component("FlowyNewBlock", FlowyNewBlock);
+  Vue.component("FlowyDragHandle", FlowyDragHandle);
 }
 
 // Create module definition for Vue.use()
@@ -26,17 +21,15 @@ const plugin = {
 
 // UMD auto-install when vue is found (eg. in browser via <script> tag)
 let GlobalVue = null;
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
+} else if (typeof global !== "undefined") {
   GlobalVue = global.Vue;
 }
 if (GlobalVue) {
   GlobalVue.use(plugin);
 }
 
-export {
-  Flowy, FlowyNode, FlowyBlock, FlowyNewBlock, FlowyDragHandle,
-};
+export { Flowy, FlowyNode, FlowyBlock, FlowyNewBlock, FlowyDragHandle };
 
 export default plugin;
