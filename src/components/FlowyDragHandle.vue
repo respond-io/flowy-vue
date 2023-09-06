d0e5e2eb3c3e4fca061dd77c18eec7a0f9ea787e
shoaibkhandev <shoaibkhandev@gmail.com>
2023-09-06 16:39:39 +0800

    Change FlowyDragHandle to vue file

---
+++ src/components/FlowyDragHandle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="draggable" :class="{ 'flowy-drag-handle': draggable }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlowyDragHandle",
+  props: {
+    draggable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+
+<style></style>
